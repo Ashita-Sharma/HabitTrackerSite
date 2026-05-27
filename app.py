f0ebb78acf82ca31ca1b263185f55de96653fd90
Ashita <ashitasharma2007@gmail.com>
2026-05-27 12:14:46 +0530

    added sqlite database for storing login details and retrieving them

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, redirect, url_for, render_template, request
-
+from flask import Flask, redirect, url_for, render_template, request, flash
+import sqlite3
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
+
 @app.route('/')
 def reroute():
     return redirect(url_for('sign_up'))
+
+connect = sqlite3.connect('database.db')
+connect.execute(
+    'CREATE TABLE IF NOT EXISTS PARTICIPANTS (username TEXT, \
+    email TEXT, password TEXT)')
+
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
@@ -19,16 +27,45 @@ def sign_up():
         email = request.form['userEmail']
         username = request.form['userId']
         password = request.form['userPassword']
+        confirm_password = request.form['ConfirmUserPassword']
+        if not email:
+            flash('Email is required', 'danger')
+            return redirect(url_for('sign_up'))
+        if not username:
+            flash('Username is required', 'warning')
+            return redirect(url_for('sign_up'))
+        if not password:
+            flash('Password field is required', 'danger')
+            return redirect(url_for('sign_up'))
+        if len(password) < 8:
+            flash("Password must be at least 8 characters", "warning")
+            return redirect(url_for('sign_up'))
 
-        print(email, username, password)
-
+        if password != confirm_password:
+            flash("Passwords do not match", "danger")
+            return redirect(url_for('sign_up'))
+        with sqlite3.connect("database.db") as users:
+            cursor = users.cursor()
+            cursor.execute("INSERT INTO PARTICIPANTS \
+            (username,email,password) VALUES (?,?,?)",
+                           (username, email, password))
+            users.commit()
         return redirect(url_for('dashboard'))
+
     return render_template('sign_up.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
+@app.route('/participants')
+def participants():
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM PARTICIPANTS')
+
+    data = cursor.fetchall()
+    return render_template("participants.html", data=data)
 
 
 # @app.route('/all_tasks', methods=['POST'])
